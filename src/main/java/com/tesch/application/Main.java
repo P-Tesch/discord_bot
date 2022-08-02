b3376abcbfadcc10586a448bf99b1171e8f02ebe
pedro tesch <pedrohenriquetesch@gmail.com>
2022-08-02 09:50:43 -0300

    Heroku deploy

--- src/main/java/com/tesch/application/Main.java
+++ src/main/java/com/tesch/application/Main.java
@@ -18,6 +18,7 @@ import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main {
@@ -27,6 +28,7 @@ public class Main {
         JDA builder = JDABuilder
         .createDefault(readId())
         .enableIntents(getIntents())
+        .setActivity(Activity.listening("Boate Azul"))
         .build();
 
         AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
