ca035b7f248f064fa8cbd92afa8c9459aa2064a7
pedro tesch <pedrohenriquetesch@gmail.com>
2022-08-05 10:08:06 -0300

    Removed setanejo 2 musicGenre

--- src/main/java/com/tesch/api/music/musicle/enums/MusicGenres.java
+++ src/main/java/com/tesch/api/music/musicle/enums/MusicGenres.java
@@ -2,7 +2,6 @@ package com.tesch.api.music.musicle.enums;
 
 public enum MusicGenres {
     SERTANEJO ("https://youtube.com/playlist?list=PL_1G4wR0BflkZJwiCHEWHmde66uVsI_lC"), 
-    SERTANEJO2 ("https://www.youtube.com/playlist?list=PL8uqnewyGmBTDkIjGigR0TFahPomoHg1K"),
     POP ("https://youtube.com/playlist?list=PL8uqnewyGmBT4Uk3SiorGyqfLkffTtXdW"),
     JPOP ("https://www.youtube.com/playlist?list=PL8uqnewyGmBQiC1elDQN_vNnYinkWoF42"),
     BRASIL2000 ("https://youtube.com/playlist?list=PL8uqnewyGmBTXHrVwCzud0o5P6V-jYlGs");
