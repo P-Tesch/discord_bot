99a61da2e2316257002cb4eebdc499d0fe05f40c
pedro tesch <pedrohenriquetesch@gmail.com>
2022-08-10 10:14:48 -0300

    Update helpEnums

--- src/main/java/com/tesch/api/HelpEnums.java
+++ src/main/java/com/tesch/api/HelpEnums.java
@@ -1,7 +1,7 @@
 package com.tesch.api;
 
 public enum HelpEnums {
-    PLAY ("Plays a youtube song. Usage: 'play -Url/Search content-'"), 
+    PLAY ("Plays a youtube song. Usage: 'play -Url or Search content-'"), 
     VOLUME ("Changes the bot audio volume. Usage: 'volume -Amount-"),
     PAUSE ("Pauses and unpauses the playing song. Usage: 'pause'"),
     DISCONNECT ("Disconnects the bot from the voice channel and clears the queue. Usage: 'disconnect'"),
@@ -10,7 +10,7 @@ public enum HelpEnums {
     CLEAR ("Clears the queue. Usage: 'clear'"),
     LOOP ("Repeats the queue indefinitely. Usage: 'loop'"),
     SHUFFLE ("Shuffles the current queue. Usage: 'shuffle'"),
-    MUSICLE ("Musicle minigame. Usage: 'musicle -genre- / 'musicle score' / 'musicle mode' / 'musicle start''"),
+    MUSICLE ("Musicle minigame. Usage: 'musicle -genre- or random' / 'musicle score' / 'musicle mode' / 'musicle start''"),
     COINFLIP ("Flips a coin, returning heads or tails. Usage: 'coinflip'"),
     ROLL ("Rolls a X sided dice Y times. Usage: 'roll YdX'");
 
