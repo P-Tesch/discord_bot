063f3570dbad2b9bf5320cdfd113d7818b258b9d
pedro tesch <pedrohenriquetesch@gmail.com>
2022-09-07 11:16:32 -0300

    Added bishop movement logic

--- src/main/java/com/tesch/api/games/chess/pieces/Bishop.java
+++ src/main/java/com/tesch/api/games/chess/pieces/Bishop.java
@@ -29,9 +29,69 @@ public class Bishop extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        // TODO Auto-generated method stub
-        return null;
+        boolean[][] possibleMoves = new boolean[this.getChessBoard().getBoard().length][this.getChessBoard().getBoard().length];
+		
+		
+		//Up - Right
+		for (int i = this.getPosition().getRow() - 1, j = this.getPosition().getColumn() + 1; i >= 0 && j < this.getChessBoard().getBoard().length; i--, j++) {
+			ChessPiece piece = (ChessPiece) this.getChessBoard().getBoard()[i][j];
+			if (piece == null) {
+				possibleMoves[i][j] = true;
+			}
+			else if (piece.getColor() != this.getColor()) {
+				possibleMoves[i][j] = true;
+				break;
+			}
+			else {
+				break;
+			}
+		}
+		
+		//Down - Right
+		for (int i = this.getPosition().getRow() + 1, j = this.getPosition().getColumn() + 1; i < this.getChessBoard().getBoard().length && j < this.getChessBoard().getBoard().length; i++, j++) {
+			ChessPiece piece = (ChessPiece) this.getChessBoard().getBoard()[i][j];
+			if (piece == null) {
+				possibleMoves[i][j] = true;
+			}
+			else if (piece.getColor() != this.getColor()) {
+				possibleMoves[i][j] = true;
+				break;
+			}
+			else {
+				break;
+			}
+		}
+		
+		//Down - Left
+		for (int i = this.getPosition().getRow() + 1, j = this.getPosition().getColumn() - 1; i < this.getChessBoard().getBoard().length && j >= 0; i++, j--) {
+			ChessPiece piece = (ChessPiece) this.getChessBoard().getBoard()[i][j];
+			if (piece == null) {
+				possibleMoves[i][j] = true;
+			}
+			else if (piece.getColor() != this.getColor()) {
+				possibleMoves[i][j] = true;
+				break;
+			}
+			else {
+				break;
+			}
+		}
+		
+		//Up - Left
+		for (int i = this.getPosition().getRow() - 1, j = this.getPosition().getColumn() - 1; i >= 0 && j >= 0; i--, j--) {
+			ChessPiece piece = (ChessPiece) this.getChessBoard().getBoard()[i][j];
+			if (piece == null) {
+				possibleMoves[i][j] = true;
+			}
+			else if (piece.getColor() != this.getColor()) {
+				possibleMoves[i][j] = true;
+				break;
+			}
+			else {
+				break;
+			}
+		}
+		
+		return possibleMoves;
     }
-
-    
 }
