c62ad1de40da5ec6a1ac6bf7b5a19060a3964c74
pedro tesch <pedrohenriquetesch@gmail.com>
2022-09-07 11:24:32 -0300

    Added Hololive music genre

--- src/main/java/com/tesch/api/games/musicle/enums/MusicGenres.java
+++ src/main/java/com/tesch/api/games/musicle/enums/MusicGenres.java
@@ -4,7 +4,8 @@ public enum MusicGenres {
     SERTANEJO ("https://youtube.com/playlist?list=PL_1G4wR0BflkZJwiCHEWHmde66uVsI_lC"), 
     POP ("https://youtube.com/playlist?list=PL8uqnewyGmBT4Uk3SiorGyqfLkffTtXdW"),
     JPOP ("https://www.youtube.com/playlist?list=PL8uqnewyGmBQiC1elDQN_vNnYinkWoF42"),
-    BRASIL2000 ("https://youtube.com/playlist?list=PL8uqnewyGmBTXHrVwCzud0o5P6V-jYlGs");
+    BRASIL2000 ("https://youtube.com/playlist?list=PL8uqnewyGmBTXHrVwCzud0o5P6V-jYlGs"),
+    HOLOLIVE ("https://youtube.com/playlist?list=PL_1G4wR0BflmDyUnNrWu50ZmkPDUg_XQ5");
 
     private String Url;
 
